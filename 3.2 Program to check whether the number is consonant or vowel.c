ecf73f21a401f058183d6d43084eeaa1bbae1483
RakshitVerma01 <147137405+RakshitVerma01@users.noreply.github.com>
2023-10-10 22:13:48 +0530

    Update 3.2 Program to check whether the number is consonant or vowel.c

--- 3.2 Program to check whether the number is consonant or vowel.c
+++ 3.2 Program to check whether the number is consonant or vowel.c
@@ -2,15 +2,15 @@
 int main()
 {
 	char ch;
-    printf("Enter any alphabet to check whether it is vowel or cosonant:-");
+    printf("Enter the alphabet");
     scanf("%s",&ch);
 	if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E'
 		|| ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
 		|| ch == 'u' || ch == 'U') {
 
-		printf("The character %c is a vowel.\n", ch);
+		printf("It is vowel.\n", ch);
 	}
 	else {
-		printf("The character %c is a consonant.\n", ch);
+		printf("It is consonent.\n", ch);
 	}
 }
